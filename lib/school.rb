ce38cb36130e6b1d96c2e4738d14276d07811f0f
Tony Rim <duc_thinh_03@yahoo.com>
2019-11-15 00:57:12 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,36 +1,40 @@
 class School
-
+  
 attr_reader :name, :roster
-
+  
 def initialize(name)
-  @name = name 
+  @name = name
   @roster = {}
-end 
-
-
+end
 
 def add_student(student, grade)
   @roster[grade] ||= []
   @roster[grade] << student
 end 
 
+def grade (level)
 
-def grade(level)
-  roster.detect do |x,y|
+  roster.map do |x,y|
     if x == level
       return y
-end 
-end
+    end 
+  end 
 end 
 
 
 def sort
-  new_hash = {}
+  hash = {}
   roster.each do |x,y|
-  new_hash[x] = y.sort
-end
-new_hash
+    hash[x] = y.sort
+  end 
+  hash
 end 
 
-    
+
 end 
+
+
+
+
+
+
